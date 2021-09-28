c3048a535bb18a82ac822900c844791b7fc25533
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-09-28 19:21:26 +0300

    Update url.py

--- url.py
+++ url.py
@@ -1,18 +1,32 @@
-
+  GNU nano 5.4                 durum.py                           
 import urllib
 from urllib import request
 import os
 import time
 
 os.system("sudo apt-get install figlet")
+os.system("clear")
 print("------------------------------")
 os.system("figlet XALE")
 print(" ")
 print("URL Durumu Kontrol Etme Aracı - URL Checker ")
 print("------------------------------")
 print(" ")
-site = input("Hedef Site: ")
+site = input("Hedef Site: (Örnek: google.com Veya www.google.com)>
+print("[1] HTTP") 
+print("[2] HTTPS")
+slct = input("Hangisi? (Default: 1) ")
 
-resp = request.urlopen(site)
+if slct == "1":
+ target = "http://" + site
+
+elif slct == "2":
+ target = "https://" + site 
+
+else:
+ print("Yanlış Seçim , Tekrar Deneyin.")
+
+resp = request.urlopen(target)
 print(resp.code)
+
 print("200 - Çalışıyor | 403 - Giriş Yasak | 400 - Geçersiz İstek | 404 - Bulunamadı , Sitede İndex Mevcut Değil.")
